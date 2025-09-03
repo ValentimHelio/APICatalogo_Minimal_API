753e1667be1743bf4ac01918625760e5c32a1d6d
valentimhelio <valentimhelio@gmail.com>
2025-09-03 17:00:00 -0400

    242. Apresentando a Fluent API 243. Usando a Fluent API para configurar as entidades

--- APICatalogo/Context/AppDBContext.cs
+++ APICatalogo/Context/AppDBContext.cs
@@ -6,8 +6,31 @@ namespace APICatalogo.Context;
 public class AppDBContext : DbContext
 {
     public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
-    
+
     public DbSet<Produto>? Produtos { get; set; }
     public DbSet<Categoria>? Categorias { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder mb)
+    {
+        //base.OnModelCreating(mb);
+
+        //Categoria
+        mb.Entity<Categoria>().HasKey(c => c.CategoriaId);
+        mb.Entity<Categoria>().Property(c => c.Nome).HasMaxLength(100).IsRequired();
+        mb.Entity<Categoria>().Property(c => c.Descricao).HasMaxLength(150).IsRequired();
+
+        //Produto
+        mb.Entity<Produto>().HasKey(c => c.ProdutoId);
+        mb.Entity<Produto>().Property(c => c.Nome).HasMaxLength(100).IsRequired();
+        mb.Entity<Produto>().Property(c => c.Descricao).HasMaxLength(150);
+        mb.Entity<Produto>().Property(c => c.Imagem).HasMaxLength(150);
+        mb.Entity<Produto>().Property(c => c.Preco).HasPrecision(14, 2);
+
+        //Relacionamento
+        mb.Entity<Produto>()
+            .HasOne<Categoria>(c => c.Categoria)
+            .WithMany(p => p.Produtos)
+            .HasForeignKey(p => p.CategoriaId);
+    }
 }
 
