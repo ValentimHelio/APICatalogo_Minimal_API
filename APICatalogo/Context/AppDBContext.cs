ffc426711d188d70c358d894bfd9191fb5132c69
valentimhelio <valentimhelio@gmail.com>
2025-09-03 16:43:52 -0400

    241. Criando a classe de Contexto

---
+++ APICatalogo/Context/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context;
+
+public class AppDBContext : DbContext
+{
+    public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+    
+    public DbSet<Produto>? Produtos { get; set; }
+    public DbSet<Categoria>? Categorias { get; set; }
+}
+
