4a018a42d842c89cba300d127862e2a8770ce991
valentimhelio <valentimhelio@gmail.com>
2025-09-10 16:19:16 -0400

    254. Testando a implementação da autenticação JWT

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +13,36 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiCatalogo", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = @"JWT Authorization header using the Bearer scheme.
+                    Enter 'Bearer'[space].Example: \'Bearer 12345abcdef\'",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference
+                              {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id = "Bearer"
+                              }
+                          },
+                         new string[] {}
+                    }
+                });
+});
+
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDBContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
@@ -55,7 +85,7 @@ app.MapPost("/login", [AllowAnonymous] (UserModel userModel, ITokenService token
     }
     else
     {
-        return Results.BadRequest("Login Inv lido");
+        return Results.BadRequest("Login Invalido");
     }
 }).Produces(StatusCodes.Status400BadRequest)
               .Produces(StatusCodes.Status200OK)
@@ -75,7 +105,7 @@ app.MapPost("/Categorias", async (Categoria categoria, AppDBContext db) =>
     return Results.Created($"/categorias/{categoria.CategoriaId}", categoria);
 }).WithName("Criar Categoria");
 
-app.MapGet("/Categorias", async (AppDBContext db) => await db.Categorias.ToListAsync()).RequireAuthorization();
+app.MapGet("/Categorias", async (AppDBContext db) => await db.Categorias.ToListAsync()).WithTags("Categoria").RequireAuthorization();
 
 app.MapGet("/Categorias/{id:int}", async (int id, AppDBContext db) =>
 {
@@ -128,7 +158,7 @@ app.MapPost("/Produtos", async (Produto produto, AppDBContext db) =>
     return Results.Created($"/produtos/{produto.ProdutoId}", produto);
 }).WithName("Criar Produto");
 
-app.MapGet("/Produto", async (AppDBContext db) => await db.Produtos.ToListAsync()).RequireAuthorization();
+app.MapGet("/Produto", async (AppDBContext db) => await db.Produtos.ToListAsync()).WithTags("Produto").RequireAuthorization();
 
 app.MapGet("/Pategorias/{id:int}", async (int id, AppDBContext db) =>
 {
